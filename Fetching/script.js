ac2065586e97474f7b6c9b5d6b72827e162f4979
Nati Melkamu <natimelkemu@gmail.com>
2025-07-27 09:26:00 -0700

    added fetching

---
+++ Fetching/script.js
@@ -0,0 +1,22 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", './movies.json', true); // true = async
+
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+
+    const data = JSON.parse(this.responseText);
+data.forEach(movie => {
+  const li = document.createElement('li');
+  li.innerHTML = `<strong>${movie.name}</strong> — ${movie.genre} (Rating: ${movie.rating})`;
+  document.querySelector('#results').appendChild(li);
+});
+
+
+    console.log("Fetched Data:", data); // Just log once
+  } else if (this.readyState === 4) {
+    // If readyState is 4 but status is not 200
+    console.error("Error fetching the file. Status:", this.status);
+  }
+};
+
+xhr.send();
